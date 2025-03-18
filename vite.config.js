3afe3a721b81ed3ae618d5364dc27abcfef2de2f
YoDaMned59 <sduvivier59112@gmail.com>
2025-03-18 10:29:57 +0100

    config vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'Mon-site-main ', // Remplace "nom-de-ton-depot" par le nom exact de ton dépôt
 })
